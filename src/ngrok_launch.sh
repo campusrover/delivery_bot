b0065181ce4626a56951626bb3c774a4075be71a
root <danielzhang@brandeis.edu>
2020-11-16 20:40:00 +0000

    add database to persistant tasks

--- src/ngrok_launch.sh
+++ src/ngrok_launch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+apt-get install sqlite3
 source ~/catkin_ws/devel/setup.bash
-cd ~/catkin_ws/src/delivery_bot/ && ./ngrok http 5000
+cd ~/catkin_ws/src/delivery_bot/ && ./ngrok http -subdomain=deliverybot 5000
--- src/voice_backend.py
+++ src/voice_backend.py
@@ -1,17 +1,33 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, Response, g
 import rospy
 from std_msgs.msg import String
+import sqlite3
+import json
 
 app = Flask(__name__)
 
+conn = sqlite3.connect('database.db')
+conn.execute('CREATE TABLE IF NOT EXISTS tasks (recipient text, phone text, status text, create_time datetime default current_timestamp)')
 
 @app.route('/')
 def backend_entrance():
     return Response(status=200)
 
+@app.before_request
+def before_request():
+    g.db = sqlite3.connect("database.db")
+
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
+
 @app.route('/deliver', methods=['GET', 'POST'])
 def deliver():
-    print(request.data)
+    deliver_data = request.get_json(force=True)
+    g.db.execute("INSERT INTO tasks (recipient, phone, status) VALUES(?, ?, ?)", (deliver_data["name"], deliver_data["phone"], 'new_task'))
+    g.db.commit()
     return "received"
 
 if __name__ == '__main__':
