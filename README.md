3bc7d28415e0781a50f03b7ce672319c6a9ed26d
BabyCube <yifei.han-123@outlook.com>
2020-11-30 21:36:20 +0000

    color check added, world changed

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ ROS Delivery Bot
 
 To make the model (including willow garage base with colored boxes as identifiers) load correctly, make sure this repo is located within ../my_ros_data/catkin_ws/src/
 
-To launch navigation with map constructed: roslaunch delivery_bot delivery_navigation.launch map_file:=/my_ros_data/catkin_ws/src/delivery_bot/map/modified_world.yaml
+To launch modified Gazebo world: roslaunch delivery_bot delivery_world.launch model:=waffle
+
+To navigate/ perform delivery, first: roslaunch delivery_bot delivery_navigation.launch map_file:=/my_ros_data/catkin_ws/src/delivery_bot/map/modified_world.yaml
+To deliviver to a single person: python src/deliver_routing.py --name Yifei
--- src/deliver_routing.py
+++ src/deliver_routing.py
@@ -6,24 +6,33 @@ import rospy
 import actionlib
 import sys, argparse
 import pickle
+import time
+import cv2, cv_bridge, numpy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+from nav_msgs.msg import Odometry
 
 parser = argparse.ArgumentParser(description='Delivery Routing')
 parser.add_argument('--name', type=str, default="Pito",
                     help='Enter the name of the person accepting delivery')
 args = parser.parse_args()
 
-jar_opener_1 = open('name_color_dict.pickle','rb')
-name_color_dict = pickle.load(jar_opener_1)
-jar_opener_1.close()
-jar_opener_2 = open('color_location_dict.pickle','rb')
-color_location_dict = pickle.load(jar_opener_2)
-jar_opener_2.close()
+# jar_opener = open('name_location_dict.pickle','rb')
+# name_color_dict = pickle.load(jar_opener_1)
+# jar_opener.close()
 
-#name_color_dict = {"Pito":"Red", "Daniel":"Green", "Yifei":"Blue"}
-#color_location_dict = {"Red":[-3.37, -10.42], "Blue":[0.92, -10.42], "Green":[-6.15, -10.42]}
+rospy.init_node('deliver_routing')
 
-def movebase_client(color_input):
+name_location_dict = {"Pito":[-3.37, -10.42], "Daniel":[0.80, -10.1], "Yifei":[-6.15, -10.42]}
+img = []
+green_box_indicator = False
+
+# define bridge for OpenCV conversion
+bridge = cv_bridge.CvBridge()
+
+def movebase_client(name_input):
 
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
     client.wait_for_server()
@@ -31,9 +40,9 @@ def movebase_client(color_input):
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = color_location_dict[color_input][0]
-    goal.target_pose.pose.position.y = color_location_dict[color_input][1]
-    goal.target_pose.pose.orientation.w = 1.0
+    goal.target_pose.pose.position.x = name_location_dict[name_input][0]
+    goal.target_pose.pose.position.y = name_location_dict[name_input][1]
+    goal.target_pose.pose.orientation.w = -1
 
     client.send_goal(goal)
     wait = client.wait_for_result()
@@ -43,13 +52,70 @@ def movebase_client(color_input):
     else:
         return client.get_result()
 
+def image_callback(msg):
+    global img
+    global green_box_indicator
+
+    cv_image = bridge.imgmsg_to_cv2(msg) #convert img to OpenCV type
+    img = cv_image
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    cv2.imshow("HSV Image", hsv)
+    cv2.waitKey(10)
+    lower_green = numpy.array([ 50, 0, 0])
+    upper_green = numpy.array([ 75, 255, 255])
+    mask = cv2.inRange(hsv, lower_green, upper_green) 
+    masked = cv2.bitwise_and(img, img, mask=mask) 
+    cv2.imshow("masked Image", masked)
+    cv2.waitKey(10)
+    M = cv2.moments(mask)
+    if M['m00'] > 0:
+        green_box_indicator = True
+    else:
+        green_box_indicator = False
+
+def rotate_check():
+    global green_box_indicator
+    if green_box_indicator:
+        return True
+    t = Twist()
+    t.linear.x = 0
+    t.angular.z = 0.5
+    pub.publish(t)
+    t_end = time.time() + 5
+    while time.time() < t_end:
+        if green_box_indicator:
+            t.angular.z = 0
+            pub.publish(t)
+            return True
+    t.angular.z = 0
+    pub.publish(t)
+    return False
+
+
+def detect_status():
+    global green_box_indicator
+    # return true if green detected (meaning the person is present)
+    # call rotate check to rotate the robot - return false if still no green 
+    if green_box_indicator:
+        return True
+    else:
+        return rotate_check()
+
+def deliver_n_check(name_input):
+    # head to location based on name, check if person there by looking for green box
+    movebase_client(name_input)
+    return detect_status()
+
+pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, image_callback) 
+
+
 if __name__ == '__main__':
     try:
-        rospy.init_node('movebase_client_py')
         print("Name Input: ", args.name)
-        print("Identity color: ", name_color_dict[args.name])
-        result = movebase_client(name_color_dict[args.name])
+        result = deliver_n_check(args.name)
         if result:
-            rospy.loginfo("Goal reached!")
+            print("the person is there!")
+            rospy.loginfo("Goal reached and the person is there!")
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation terminated.")
