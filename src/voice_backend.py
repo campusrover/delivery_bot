0941b339884b77a3e93996885bbc5ec073960a84
root <danielzhang@brandeis.edu>
2020-12-01 03:34:36 +0000

    add ability to determine whether robots are available and take tasks. add multi robots

--- src/voice_backend.py
+++ src/voice_backend.py
@@ -1,17 +1,29 @@
-from flask import Flask, request, Response, g
-import rospy
-from std_msgs.msg import String
+from flask import Flask, request, Response, g, jsonify
 import sqlite3
 import json
+
+import rospy
+from std_msgs.msg import String
 
 app = Flask(__name__)
 
 conn = sqlite3.connect('database.db')
-conn.execute('CREATE TABLE IF NOT EXISTS tasks (recipient text, phone text, status text, create_time datetime default current_timestamp)')
+conn.execute('CREATE TABLE IF NOT EXISTS tasks (recipient text, phone text, status text, create_time datetime default current_timestamp, robot_id int)')
+conn.commit()
+conn.execute('CREATE TABLE IF NOT EXISTS robots (id int primary key, status text)')
+conn.commit()
 
 @app.route('/')
 def backend_entrance():
-    return Response(status=200)
+    robot_status = g.db.execute("select * from robots where status='idle'").fetchall()
+    if len(robot_status) > 0:
+        return jsonify(
+            available = True
+        )
+    else:
+        return jsonify(
+        available = False
+        )
 
 @app.before_request
 def before_request():
@@ -26,9 +38,12 @@ def teardown_request(exception):
 @app.route('/deliver', methods=['GET', 'POST'])
 def deliver():
     deliver_data = request.get_json(force=True)
-    g.db.execute("INSERT INTO tasks (recipient, phone, status) VALUES(?, ?, ?)", (deliver_data["name"], deliver_data["phone"], 'new_task'))
+    
+    selected_robot = g.db.execute("select id from robots where status='idle' order by random() limit 1").fetchall()[0][0]
+    g.db.execute("INSERT INTO tasks (recipient, phone, status, robot_id) VALUES(?, ?, ?, ?)", (deliver_data["name"], deliver_data["phone"], 'new_task', selected_robot))
     g.db.commit()
     return "received"
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
