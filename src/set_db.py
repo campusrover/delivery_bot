6b3a64cf73897bfd3d009171ef08afd59c04ef25
root <danielzhang@brandeis.edu>
2020-12-09 04:22:45 +0000

    optimized databse initialization file

--- src/set_db.py
+++ src/set_db.py
@@ -2,6 +2,8 @@ import sqlite3
 
 conn = sqlite3.connect('database.db')
 
+
+# create table
 conn.execute('CREATE TABLE IF NOT EXISTS tasks (recipient_id int, status text, create_time datetime default current_timestamp, robot_id int)')
 conn.commit()
 conn.execute('CREATE TABLE IF NOT EXISTS robots (id int primary key, status text, update_time datetime default current_timestamp)')
@@ -9,9 +11,23 @@ conn.commit()
 conn.execute('CREATE TABLE IF NOT EXISTS employees (id int primary key, name text, location_x real, location_y real )')
 conn.commit()
 
-conn.execute("insert into employees values(1, 'Pito', -3.37, -10.42)")
+# clear old data
+conn.execute("delete from employees")
+conn.commit()
+conn.execute("delete from robots")
+conn.commit()
+conn.execute("delete from tasks")
 conn.commit()
 
+
+# initialize robot data
+conn.execute("insert into robots (id, status) values(1, 'idle')")
+conn.commit()
+conn.execute("insert into robots (id, status) values(2, 'idle')")
+conn.commit()
+
+
+# initialize employee data
 conn.execute("insert into employees values(1, 'Pito', -3.37, -10.42)")
 conn.commit()
 conn.execute("insert into employees values(2, 'Daniel', 0.08, -10.1)")
